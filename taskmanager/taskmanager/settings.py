25d7314437f8bf8cb5895f7d2d83fedd7576f15c
Sandfin Nasio <nasiosandfin@gmail.com>
2026-02-28 14:28:45 +0300

    Ready for PythonAnywhere deployment

--- taskmanager/taskmanager/settings.py
+++ taskmanager/taskmanager/settings.py
@@ -118,10 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-import os
-BASE_DIR = Path(__file__).resolve().parent.parent
+DEBUG = False
+ALLOWED_HOSTS = ['yourusername.pythonanywhere.com']
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 
